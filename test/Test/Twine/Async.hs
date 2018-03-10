7e377c99b898a84192def51499458b4b647bdf20
nhibberd <nhibberd@gmail.com>
2018-03-10 18:34:20 +0700

    Move Async tests to hedgehog

--- test/Test/Twine/Async.hs
+++ test/Test/Twine/Async.hs
@@ -5,14 +5,13 @@ module Test.Twine.Async where
 
 import           Control.Concurrent.Async (async, wait, cancel)
 import           Control.Monad.Catch (catchAll)
+import           Control.Monad.IO.Class (liftIO)
 
-import           Disorder.Core.IO
+import           Hedgehog
 
 import           P
 
 import           System.IO
-
-import           Test.QuickCheck
 
 import           Twine.Async
 import           Twine.Data
@@ -20,23 +19,29 @@ import           Twine.Snooze (snooze)
 
 import           X.Control.Monad.Trans.Either
 
-prop_wait_no_timeout = testIO $ do
-  a <- async . snooze $ microseconds 5
-  e <- runEitherT $ waitWithTimeout a (microseconds 10)
-  pure $ e === Right ()
+prop_wait_no_timeout :: Property
+prop_wait_no_timeout =
+  property $ do
+    a <- liftIO . async . snooze $ microseconds 5
+    e <- liftIO . runEitherT $ waitWithTimeout a (microseconds 10)
+    e === Right ()
 
-prop_wait_timeout = testIO $ do
-  a <- async $ (snooze $ milliseconds 20)
-  e <- runEitherT $ waitWithTimeout a (milliseconds 5)
-  pure $ e === Left (AsyncTimeout (milliseconds 5))
+prop_wait_timeout :: Property
+prop_wait_timeout =
+  property $ do
+    a <- liftIO . async $ (snooze $ milliseconds 20)
+    e <- liftIO . runEitherT $ waitWithTimeout a (milliseconds 5)
+    e === Left (AsyncTimeout (milliseconds 5))
 
-prop_cancel_outside = testIO $ do
-  a <- async $ (snooze (milliseconds 20) >> pure True)
-  e <- async $ runEitherT $ waitWithTimeout a (milliseconds 5)
-  cancel a
-  r <- wait e `catchAll` (\_ -> pure . Right $ False)
-  pure $ r === Right False
+prop_cancel_outside :: Property
+prop_cancel_outside =
+  property $ do
+    a <- liftIO . async $ (snooze (milliseconds 20) >> pure True)
+    e <- liftIO . async $ runEitherT $ waitWithTimeout a (milliseconds 5)
+    liftIO $ cancel a
+    r <- liftIO $ wait e `catchAll` (\_ -> pure . Right $ False)
+    r === Right False
 
-return []
 tests :: IO Bool
-tests = $forAllProperties $ quickCheckWithResult (stdArgs { maxSuccess = 10 })
+tests =
+  checkSequential $$(discover)
